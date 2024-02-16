574ef688c65d565951f3af14cf7fac161498b331
David <40817david@gmail.com>
2024-02-16 16:53:21 +0800

    still working

--- src/hw6/Caculator.java
+++ src/hw6/Caculator.java
@@ -1,32 +1,31 @@
 package hw6;
 
 public class Caculator {
-	
+
 	private int x;
 	private int y;
-	
+
 	public Caculator() {
 		super();
 	}
-	public Caculator(int x, int y) {
+
+	public Caculator(int x, int y) throws CalException {
 		setXY(x, y);
 	}
-	
-	public void setXY(int x, int y) {
-		if() {
-			
-		}else if() {
-			
-		}else if() {
-			
-		}else {
-		this.x = x;
-		this.y = y;
+
+	public void setXY(int x, int y) throws CalException {
+		if (x == 0 && y == 0) {
+			throw new CalException("0的0次方沒有意義!");
+		} else if (y < 0) {
+			throw new CalException("次方為負值，結果回傳不為整數!");
+		}  else {
+			this.x = x;
+			this.y = y;
 		}
 	}
-	
-	public int powerXY(int x, int y) {
-		int power = x * y;
+
+	public int powerXY() {
+		int power =(int)(Math.pow(x, y));
 		return power;
 	}
 }
--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -1,22 +1,32 @@
 package hw6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalTest {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("請輸入x的值:");
-		int x = sc.nextInt();
-		System.out.println("請輸入y的值:");
-		int y = sc.nextInt();
-		
-		try{
-			Caculator c = new Caculator(x,y);
-			System.out.println(x+"的"+y+"次方等於"+c.powerXY(x, y));
-		}catch(CalException e) {
-			e.printStackTrace();
+		int x, y;
+
+		while (true) {
+			try {
+				System.out.println("請輸入x的值:");
+				x = sc.nextInt();
+				System.out.println("請輸入y的值:");
+				y = sc.nextInt();
+
+				Caculator c = new Caculator(x, y);
+				System.out.println("=========================");
+				System.out.println(x + "的" + y + "次方等於" + c.powerXY());
+				break;
+			} catch (CalException e) {
+				System.out.println(e.getMessage());
+				continue;
+			} catch (RuntimeException re) {
+				System.out.println("輸入格式不正確");
+				continue;
+			}
 		}
 	}
-
 }
