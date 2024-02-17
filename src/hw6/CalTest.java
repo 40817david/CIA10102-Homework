d7475ceca70b17b33c0aa12f475d74af17d93ea0
David <40817david@gmail.com>
2024-02-17 10:39:41 +0800

    Hw6 done

--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -6,26 +6,27 @@ import java.util.Scanner;
 public class CalTest {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		int x, y;
-
+		Caculator c = new Caculator();
+		
 		while (true) {
 			try {
+				Scanner sc = new Scanner(System.in);
 				System.out.println("請輸入x的值:");
 				x = sc.nextInt();
 				System.out.println("請輸入y的值:");
 				y = sc.nextInt();
 
-				Caculator c = new Caculator(x, y);
+				c.setXY(x, y);
 				System.out.println("=========================");
 				System.out.println(x + "的" + y + "次方等於" + c.powerXY());
 				break;
 			} catch (CalException e) {
 				System.out.println(e.getMessage());
-				continue;
-			} catch (RuntimeException re) {
+				
+			} catch (InputMismatchException ime) {
 				System.out.println("輸入格式不正確");
-				continue;
+				
 			}
 		}
 	}
