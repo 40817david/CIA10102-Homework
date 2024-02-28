a64e909867aa5b2c5c4e74dd917c99bd191e5f59
David <40817david@gmail.com>
2024-02-28 15:23:55 +0800

    Hw9 done

--- src/hw9/Hw9_2.java
+++ src/hw9/Hw9_2.java
@@ -1,14 +1,14 @@
 package hw9;
 
 class Account {
-	static int save = 0;
+	 int save = 0;
 
 	synchronized public void deposit(int money) {
 		while (save > 3000) {
 			System.out.println("媽媽看到餘額在3000以上，暫停匯款");
 			try {
-				wait();
 				System.out.println("熊大被老媽告知帳戶已經有錢!");
+				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -19,11 +19,11 @@ class Account {
 	}
 
 	synchronized public void withdraw(int money) {
-		while (save < money) {
+		while (save == 0) {
 			System.out.println("熊大看到帳戶沒錢，暫停提款");
 			try {
-				wait();
 				System.out.println("媽媽被熊大要求匯款!");
+				wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -32,8 +32,8 @@ class Account {
 		System.out.println("熊大領了" + money + "，帳戶共有:" + save);
 		if (save < 2000) {
 			System.out.println("熊大看到餘額在2000以下，要求匯款");
-			notify();
 		}
+		notify();
 	}
 }
 
@@ -58,7 +58,7 @@ class Son extends Thread {
 		this.account = account;
 	}
 
-	public void run(Account account) {
+	public void run() {
 		for (int i = 0; i < 10; i++) {
 			account.withdraw(1000);
 		}
@@ -67,11 +67,10 @@ class Son extends Thread {
 
 public class Hw9_2 {
 	public static void main(String[] args) {
-		Account a1 = new Account();
-		Account a2 = new Account();
+		Account a = new Account();
 		
-		Mom mom = new Mom(a1);
-		Son son = new Son(a2);
+		Mom mom = new Mom(a);
+		Son son = new Son(a);
 	
 		mom.start();
 		son.start();
